cf12f7b3956cfef278071129bfe91e873cdf5c0a
Michael Carr <xyeres@me.com>
2022-01-28 15:02:43 -0800

    Progress before major refactor

--- src/lib/uploadToFirestore.js
+++ src/lib/uploadToFirestore.js
@@ -2,47 +2,55 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "./firebase";
 
 export default function uploadToFirestore(fileList, directory, setStatus) {
-    for (const file of fileList) {
-      const fileRef = ref(storage, `${directory}${file.name}`)
-      const uploadTask = uploadBytesResumable(fileRef, file)
+  const urls = []
 
-      setStatus([])
+  for (const file of fileList) {
+    const fileRef = ref(storage, `${directory}${file.name}`)
+    const uploadTask = uploadBytesResumable(fileRef, file)
 
-      let initialState = {
-        progress: 0,
-        isLoading: true,
-        name: file.name
-      }
+    setStatus([])
 
-      setStatus(prevState => [...prevState, initialState])
-
-      uploadTask.on('state_changed',
-        (snapshot) => {
-          let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-          setStatus(prevState => {
-            let updateExistingState = prevState.map((obj) => {
-              if (obj.name === snapshot.ref.name) return { ...obj, progress }
-              return obj
-            })
-            return [...updateExistingState]
-          })
-
-        },
-        (error) => {
-          console.error(error);
-        },
-
-        // On upload complete:
-        () => {
-          setStatus(prevState => {
-            let newState = prevState.map((obj) => ({ ...obj, isLoading: false }))
-            return [...newState]
-          })
-          getDownloadURL(uploadTask.snapshot.ref)
-            .then((url) => {
-              console.log(`File available at: ${url}`);
-            })
-        })
+    let initialState = {
+      progress: 0,
+      isLoading: true,
+      name: file.name
     }
+
+    setStatus(prevState => [...prevState, initialState])
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        setStatus(prevState => {
+          let updateExistingState = prevState.map((obj) => {
+            if (obj.name === snapshot.ref.name) return { ...obj, progress }
+            return obj
+          })
+          return [...updateExistingState]
+        })
+
+      },
+      (error) => {
+        console.error(error);
+      },
+
+      // On upload complete:
+      () => {
+        setStatus(prevState => {
+          let newState = prevState.map((obj) => ({ ...obj, isLoading: false }))
+          return [...newState]
+        })
+
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((url) => {
+            setStatus(prevState => {
+              let newState = prevState.map((obj) => ({ ...obj, url }))
+              return [...newState]
+            })
+
+            console.log(`File available at: ${url}`);
+          })
+      })
   }
+}
