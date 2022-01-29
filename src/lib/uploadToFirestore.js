290d0da39d2445f672cf6291178e8294f85ce996
Michael Carr <xyeres@me.com>
2022-01-28 19:43:48 -0800

    Rewrite upload function to be promise driven

--- src/lib/uploadToFirestore.js
+++ src/lib/uploadToFirestore.js
@@ -1,26 +1,27 @@
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-// import extractID3Tags from "./extractID3Tags";
+import { v4 as uuidv4 } from 'uuid';
 import { storage } from "./firebase";
 
-export default async function uploadFile(file, path) {
-  let url = null, fullPath = null
-  const filePath = `${path}${file.name}`
-  const fileRef = ref(storage, filePath)
-  const task = uploadBytesResumable(fileRef, file)
+export default async function uploadToFirestore(file, path) {
+  return new Promise((resolve, reject) => {
+    let url = null, metadata = null
+    const filePath = `${path}${uuidv4()}${file.name}`
+    const fileRef = ref(storage, filePath)
+    const task = uploadBytesResumable(fileRef, file)
 
-  task.on('state_changed',
-    (snapshot) => {
-      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log(progress);
-    },
-    (error) => {
-      console.error(error);
-    },
-    () => {
-
-    }
-  )
-  // fullPath = task.snapshot.metadata.fullPath
-  url = await getDownloadURL(task.snapshot.ref)
-  return { url, fullPath }
+    task.on('state_changed',
+      (snapshot) => {
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(progress);
+      },
+      (error) => {
+        reject(error)
+      },
+      async () => {
+        url = await getDownloadURL(task.snapshot.ref)
+        metadata = task.snapshot.metadata
+        resolve({ url, metadata})
+      }
+    )
+  })
 }
