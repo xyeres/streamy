52d083671b1e9e3f14df95bb2f0a507f8faf25a4
Michael Carr <xyeres@me.com>
2022-01-28 19:41:29 -0800

    Initial working peices

--- src/lib/uploadMultipleSongs.js
+++ src/lib/uploadMultipleSongs.js
@@ -1,9 +1,15 @@
-async function uploadMultipleSongsWithTags(songsWithTags) {
+import uploadFile from "./uploadToFirestore";
+
+export default async function uploadMultipleSongs(songsWithTags) {
   for (const file of songsWithTags) {
     try {
-      const audioUrl = await uploadSong(file.audio)
-      const coverUrl = await uploadCover(file.cover.file)
-      const docRef = await createDocument(file.tags, audioUrl, coverUrl)
+      const audioUpload = await uploadFile(file.audio, 'songs/')
+      const coverUpload = await uploadFile(file.cover.file, 'covers/')
+      
+      // Create documents in Firestore, add SRC urls to uploads
+      // for future reference
+      // const docRef = await createDocument(file.tags, audioUrl, coverUrl)
+
     } catch (err) {
       console.log(err);
     }
