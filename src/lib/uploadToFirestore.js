8286c70b4bd3935cfbb89566bf609a9c404517f1
Michael Carr <xyeres@me.com>
2022-01-31 14:52:21 -0800

    Add status messages

--- src/lib/uploadToFirestore.js
+++ src/lib/uploadToFirestore.js
@@ -2,26 +2,34 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { v4 as uuidv4 } from 'uuid';
 import { storage } from "./firebase";
 
-export default async function uploadToFirestore(file, path) {
+export default function uploadToFirestore(file, path, options = null) {
   return new Promise((resolve, reject) => {
-    let url = null, metadata = null
-    const filePath = `${path}${uuidv4()}${file.name}`
-    const fileRef = ref(storage, filePath)
-    const task = uploadBytesResumable(fileRef, file)
+    try {
+      let setStatus
+      if (options.setStatus) setStatus = options.setStatus
 
-    task.on('state_changed',
-      (snapshot) => {
-        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log(progress);
-      },
-      (error) => {
-        reject(error)
-      },
-      async () => {
-        url = await getDownloadURL(task.snapshot.ref)
-        metadata = task.snapshot.metadata
-        resolve({ url, metadata})
-      }
-    )
+      let url = null, metadata = null
+      const filePath = `${path}${uuidv4()}${file.name}`
+      const fileRef = ref(storage, filePath)
+      const task = uploadBytesResumable(fileRef, file)
+
+      task.on('state_changed',
+        (snapshot) => {
+          let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          if (setStatus) setStatus(prevState => ({ ...prevState, item: file.name, message: `${progress.toFixed(0)}% complete` }))
+        },
+        (error) => {
+          if (setStatus) setStatus(prevState => ({ ...prevState, item: file.name, message: `Error, please try again` }))
+          reject(error)
+        },
+        async () => {
+          url = await getDownloadURL(task.snapshot.ref)
+          metadata = task.snapshot.metadata
+          resolve({ url, metadata })
+        }
+      )
+    } catch(err) {
+      throw err;
+    }
   })
 }
