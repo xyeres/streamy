3ff2df307eee90b551b0f53ce680c995b97c1492
Michael Carr <xyeres@me.com>
2022-02-02 11:25:13 -0800

    Add songs to album document

--- src/features/AlbumList/createOrGetAlbumDoc.js
+++ src/features/AlbumList/createOrGetAlbumDoc.js
@@ -3,7 +3,7 @@ import { db } from "../../lib/firebase";
 // Creates album document in Firestore using
 // album id
 
-export default function createOrGetAlbumDoc(albumId, albumTitle, coverUrl) {
+export default function createOrGetAlbumDoc(albumId, albumTitle, coverUrl, songId) {
   return new Promise(async (resolve, reject) => {
     try {
       const docRef = doc(db, "albums", albumId)
@@ -11,6 +11,7 @@ export default function createOrGetAlbumDoc(albumId, albumTitle, coverUrl) {
         id: docRef.id,
         title: albumTitle,
         coverUrl,
+        songs: arrayUnion(songId),
         lastUpdated: serverTimestamp()
       }
       const result = await setDoc(docRef, data, { merge: true })
