1478b8920288a4e796b91cb56481eae7d22aa447
Michael Carr <xyeres@me.com>
2022-02-01 16:11:42 -0800

    Move script location

--- src/features/Uploader/UploaderListItem.js
+++ src/features/Uploader/UploaderListItem.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
-import uploadAudioTrack from "../../lib/uploadAudioTrack";
+import uploadAudioTrack from "./uploadAudioTrack";
 
 
 export default function UploaderListItem({ file }) {
