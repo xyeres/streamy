8fcfd1f81f26c93b47abbd0e8c6a7cb75b692deb
Michael Carr <xyeres@me.com>
2022-02-02 11:24:45 -0800

    Add new aspect ratios

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        '1/2': '1 / 2',
+        '1/3': '1 / 3',
+        '2/3': '2 / 3',
+        '3/4': '3 / 4',
+      },
+    },
   },
   plugins: [],
 }
