cf09bc2a0c8ccb43227b751f920dbfe2b87fe8ff
Michael Carr <xyeres@me.com>
2022-02-03 10:08:54 -0800

    Add error handling

--- src/features/Playlist/Playlist.js
+++ src/features/Playlist/Playlist.js
@@ -2,27 +2,38 @@ import PlaylistItem from "./PlaylistItem";
 import { useEffect, useState } from "react";
 import querySongs from "./querySongs";
 import { useParams } from "react-router-dom";
+import { getAlbum } from "../AlbumList/getAlbums";
+import ErrorMessage from "../Home/ErrorMessage";
+import GoBack from "../Home/GoBack";
 
 export default function Playlist() {
   const params = useParams()
   const { playlistId } = params
 
-  console.log(playlistId);
-
   const [songs, setSongs] = useState([]);
   const [firstSong, setFirstSong] = useState({});
+  const [album, setAlbum] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ status: false, message: null });
 
   useEffect(() => {
-    const fetchSongs = async (albumId) => {
-      setIsLoading(true)
-      const data = await querySongs(albumId)
-      setSongs(data)
-      setFirstSong(data[0])
-      setIsLoading(false)
+    const fetchData = async (albumId) => {
+      try {
+        setError(prevState => ({...prevState, status:false}))
+        setIsLoading(true)
+        const songs = await querySongs(albumId)
+        const album = await getAlbum(albumId)
+        setSongs(songs)
+        setFirstSong(songs[0])
+        setAlbum(album)
+      } catch (err) {
+        setError({ status: true, message: err.message })
+      } finally {
+        setIsLoading(false)
+      }
     }
 
-    fetchSongs(playlistId)
+    fetchData(playlistId)
   }, [playlistId]);
 
   const playlistItems = songs.map((song, index) => {
@@ -30,11 +41,18 @@ export default function Playlist() {
   })
 
   return (
-      <div className="p-4 w-full flex flex-col items-center">
-        <img src={firstSong.coverUrl} className="rounded-lg mb-5" />
-      <ul className="divide-y divide-solid max-w-lg w-full divide-neutral-100 pb-24 sm:pb-0">
-          {isLoading ? "Loading songs..." : playlistItems}
-        </ul>
-      </div>
+    <div className="p-4 w-full flex flex-col items-center">
+      {error.status ? <ErrorMessage message={error.message} /> : (
+        <>
+          <GoBack />
+          <img alt={`${album.title} album cover`} src={album.coverUrl} className="rounded-lg mb-5" />
+          <h1 className="font-bold text-lg">{album.title}</h1>
+          <p className="text-sm mb-3">{firstSong.artist}</p>
+          <ul className="divide-y divide-solid max-w-lg w-full divide-neutral-300 pb-24 sm:pb-0">
+            {isLoading ? "Loading songs..." : playlistItems}
+          </ul>
+        </>
+      )}
+    </div>
   )
 }
