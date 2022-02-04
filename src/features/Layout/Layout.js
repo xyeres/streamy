b291d6026e19a2da7ae58a546591bd3315402117
Michael Carr <xyeres@me.com>
2022-02-04 10:24:53 -0800

    Add menu open state

--- src/features/Layout/Layout.js
+++ src/features/Layout/Layout.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import MenuBar from "../MenuBar/MenuBar";
-import Controls from "../Player/Controls";
+import Player from "../Player/Player";
 
 export default function Layout() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="relative">
-      <main className='flex flex-col sm:flex-row gap-2 sm:gap-7 pb-28'>
+    <div className="relative w-screen h-screen">
+      <main className="w-full h-full flex flex-col sm:flex-row gap-2 sm:gap-7">
         <Outlet />
       </main>
-      <Controls />
+      <Player open={open} setOpen={setOpen} />
       <MenuBar />
     </div>
   );
