2a1749c2de73f9988fb22b93288d18a4c82a8c4a
Michael Carr <xyeres@me.com>
2022-02-14 15:05:29 -0800

    Add padding

--- src/features/AlbumList/FeaturedCard.js
+++ src/features/AlbumList/FeaturedCard.js
@@ -3,8 +3,8 @@ import { MdFiberNew } from 'react-icons/md'
 
 export default function FeaturedCard({ item }) {
   return (
-    <div className="flex cursor-pointer justify-start group hover:bg-opacity-[65%] items-start m-4 sm:mx-0 bg-black bg-opacity-10 p-4 rounded-lg">
-      <img src={CoverImage} alt="Featured item artwork" className="rounded-xl w-24 aspect-square" />
+    <div className="flex cursor-pointer justify-start group hover:bg-opacity-[65%] items-start m-4 sm:mx-4 bg-black bg-opacity-10 p-4 rounded-lg">
+      <img src={CoverImage} alt="Featured item artwork" className="rounded-xl max-w-[96px] sm:max-w-[210px] aspect-square" />
       <div className="ml-4 mt-1 group-hover:text-white">
         <div className="flex flex-row items-start">
           <MdFiberNew size="1.35em" className="group-hover:text-white inline mt-1 mr-1" />
