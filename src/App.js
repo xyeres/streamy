3d9a4d511e458e69735299f552383e6efcdfe5a6
Michael Carr <xyeres@me.com>
2022-02-14 15:05:23 -0800

    Test various data models

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import UploadDashboard from "./features/Uploader/UploadDashboard";
 import Home from "./features/Home/Home";
 import Playlist from "./features/Playlist/Playlist";
 import Layout from "./features/Layout/Layout";
+import Fb from './features/Fb.js'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/fb" element={<Fb />} />
           <Route path="/play" element={<Home />} />
           <Route path="play/:playlistId" element={<Playlist />} />
         </Route>
--- src/features/AlbumList/useAlbums.js
+++ src/features/AlbumList/useAlbums.js
@@ -1,4 +1,4 @@
-import { query, orderBy, collection, getDocs, doc, getDoc, where } from "firebase/firestore";
+import { query, orderBy, collection, getDocs, doc, getDoc, where, collectionGroup } from "firebase/firestore";
 import { db } from "../../lib/firebase";
 import useSWR from "swr";
 
@@ -29,10 +29,79 @@ export function useAlbumSongs(albumId) {
   }
 }
 
+export function useAlbumsTesting(albumId) {
+  const { data, error } = useSWR(albumId, albumsTesting)
+  return {
+    data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
+
 /*
   Fetchers, custom data fetchers for SWR custom hooks
 */
 
+export async function albumsTesting(albumId) {
+  // get an album
+  const docRef = doc(db, `albums/${albumId}`)
+  const docSnap = await getDoc(docRef)
+
+  if (docSnap.exists()) {
+    // const data = docSnap.data()
+    // const q = query(collection(db, 'songs'), where('id', 'in', data.songs))
+
+    // const qSnapshot = await getDocs(q)
+    // const songs = []
+    // qSnapshot.forEach((doc) => songs.push(doc.data()))
+
+    // get an albums songs
+    const album = docSnap.data()
+    const q = query(collection(db, `albums/${albumId}/songs`), orderBy('title'))
+
+    const qSnapshot = await getDocs(q)
+    const songs = []
+    qSnapshot.forEach((doc) => songs.push(doc.data()))
+
+
+    // Get a playlist and all of its songs
+    const playlist = []
+    const playlistDocRef = doc(db, 'playlists/EOsTVelueb8nchPRUvug')
+    const playlistSnap = await getDoc(playlistDocRef)
+    const playlistToQuery = playlistSnap.data()
+    if (!playlistSnap.exists()) throw new Error('No playlist available')
+    const playlistQuery = query(collectionGroup(db, 'songs'), where('id', 'in', playlistToQuery.songs))
+    const playlistQSnap = await getDocs(playlistQuery)
+    playlistQSnap.forEach((doc) => playlist.push(doc.data()))
+
+    return { album, songs, playlist }
+
+  } else throw new Error(`URL for "${albumId}" does not exist.`)
+}
+
+/*
+export async function albumsTesting(albumId) {
+  const docRef = doc(db, `albums/${albumId}`)
+  const docSnap = await getDoc(docRef)
+
+  if (docSnap.exists()) {
+    const data = docSnap.data()
+    const songs = await Promise.all(
+      await data.songs.map(async (songId) => {
+        const songRef = doc(db, `songs/${songId}`)
+        const docSnap = await getDoc(songRef)
+
+        if (docSnap.exists()) {
+          return docSnap.data()
+        }
+      }))
+
+    return { album: data, songs }
+
+  } else throw new Error(`URL for "${albumId}" does not exist.`)
+}
+
+*/ 
 export async function albumsFetcher() {
   const qRef = collection(db, "albums")
   const q = query(qRef, orderBy("lastUpdated"))
@@ -59,3 +128,4 @@ export async function albumSongsFetcher(albumId) {
   if (docsBuffer.length > 0) return docsBuffer
   else throw new Error(`Unable to fetch songs for ${albumId}`)
 }
+
