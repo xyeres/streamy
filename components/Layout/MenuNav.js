93d0fdb3c3d154be1a562edac930ebcd98b6fb94
Michael Carr <xyeres@me.com>
2022-02-18 09:46:39 -0800

    Remove useless css class

--- components/Layout/MenuNav.js
+++ components/Layout/MenuNav.js
@@ -6,7 +6,7 @@ export default function MenuNav({ home, search, library }) {
     return page ? "drop-shadow-lg text-gray-900" : "text-gray-700"
   };
   return (
-    <div className="flex px-4 items-center justify-around h-12 border-t border-gray-300 bg-white fixed scroll bottom-0 left-0 right-0">
+    <div className="flex px-4 items-center justify-around h-12 border-t border-gray-300 bg-white fixed bottom-0 left-0 right-0">
       <Link href="/">
         <a>
           <MdHomeFilled size="1.25em" className={`${homeIsActive(home)}`} />
