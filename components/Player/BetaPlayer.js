35c0cd8f6a1cdd65a54bbe53a392a547c720879a
Michael Carr <xyeres@me.com>
2022-02-21 16:51:16 -0800

    sliding menu

--- components/Player/BetaPlayer.js
+++ components/Player/BetaPlayer.js
@@ -1,32 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 import { useSelector } from 'react-redux';
 
 export default function BetaPlayer({ playerRef }) {
   const src = useSelector(state => state.player.betaUrl)
-  console.log('player src', src)
+  const [isOpen, setIsOpen] = useState(false)
 
+  if (playerRef.current) {
+    var audioEl = playerRef.current.audioEl.current;
+  }
   useEffect(() => {
 
     if (src != null) {
-      playerRef.current.audioEl.current.src = src
-      playerRef.current.audioEl.current.load()
-      playerRef.current.audioEl.current.play()
+      audioEl.src = src
+      audioEl.title = 'This is the Song Title!'
+      audioEl.load()
+
+      audioEl.play()
     }
 
   }, [src])
 
-  const handlePlayPause = () => {
-    if (playerRef.current.audioEl.current.paused === true) {
-      playerRef.current.audioEl.current.play()
-    } else {
-      playerRef.current.audioEl.current.pause()
+  const handlePlayPause = (e) => {
+    e.stopPropagation();
+
+    console.log(audioEl)
+    if (audioEl) {
+      if (audioEl.paused === true) {
+        audioEl.play()
+      } else {
+        audioEl.pause()
+      }
     }
   }
 
+  const handlePlayerOpen = (e) => {
+    e.stopPropagation();
+    setIsOpen(prevState => !prevState)
+  }
+
   return (
-    <div className="bottom-24 p-12 right-0 left-0 fixed flex justify-center items-center bg-opacity-80 bg-black">
-      <button onClick={handlePlayPause} className="bg-slate-200 p-3 py-5">Play/Pause</button>
+    <div onClick={handlePlayerOpen} className={`${isOpen ? 'translate-x-0' : 'translate-x-[96%]'} transition-transform duration-500 cursor-zoom-in bottom-44 p-4 right-0 left-0 fixed flex justify-center items-center bg-opacity-80 bg-black`}>
+      <button onClick={handlePlayPause} className="bg-slate-200 p-1 py-3">play/paus</button>
       <ReactAudioPlayer
         ref={playerRef}
         src={src}
