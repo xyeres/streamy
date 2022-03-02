d404f16ca88722fdecdcad9b084cdfb15524fb20
Michael Carr <xyeres@me.com>
2022-03-02 10:06:44 -0800

    Add og:description

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,8 @@ function MyApp({ Component, pageProps }) {
     <Provider store={store}>
     <Head>
         <title>Stream United Pursuit and friends</title>
-        <meta property="og:title" content="Stream music from United Pursuit and friends" />
+        <meta property="og:title" content="Stream music from United Pursuit" />
+        <meta property="og:description" content="A growing music library of albums and exclusive tracks from United Pursuit and friends" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://streamy-nu.vercel.app" />
         <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/streamy-dev-51f11.appspot.com/o/static%2Fimages%2Fog%2FStreamyOGImage-1%400.75x.jpg?alt=media&token=dc57d86e-0b2c-4a7f-8913-d2ea3dd9a7fb" />
