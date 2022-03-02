b6ebfe4bf1cd66157cc87d0636b334f51e8e96ab
Michael Carr <xyeres@me.com>
2022-03-02 13:14:52 -0800

    Update desc

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,12 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@unitedpursuit" />
         <meta name="twitter:title" content="Stream music from United Pursuit" />
-        <meta name="twitter:description" content="Listen to a growing music library of albums and exclusive tracks from United Pursuit and friends" />
+        <meta name="twitter:description" content="Listen to a growing library of albums and exclusive tracks from United Pursuit and friends" />
         <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/streamy-dev-51f11.appspot.com/o/static%2Fimages%2Fog%2FStreamyOGImage-1%400.75x.jpg?alt=media&token=dc57d86e-0b2c-4a7f-8913-d2ea3dd9a7fb" />
 
         <meta property="og:title" content="Stream music from United Pursuit" />
         <meta property="og:site_name" content="United Pursuit" />
-        <meta property="og:description" content="Listen to a growing music library of albums and exclusive tracks from United Pursuit and friends" />
+        <meta property="og:description" content="Listen to a growing library of albums and exclusive tracks from United Pursuit and friends" />
         <meta property="og:type" content="object" />
         <meta property="og:url" content="https://streamy-nu.vercel.app" />
         <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/streamy-dev-51f11.appspot.com/o/static%2Fimages%2Fog%2FStreamyOGImage-1%400.75x.jpg?alt=media&token=dc57d86e-0b2c-4a7f-8913-d2ea3dd9a7fb" />
