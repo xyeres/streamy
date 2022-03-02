abec87c53e0f2eee88e8334a01f8fec5c902d804
Michael Carr <xyeres@me.com>
2022-03-01 20:23:04 -0800

    Add styles to gridCover container

--- components/CoverGrid/CoverGrid.js
+++ components/CoverGrid/CoverGrid.js
@@ -20,7 +20,7 @@ function CoverGrid({ coll, path, order, limit, swrkey, keywords }) {
   })
 
   return (
-    <div className="flex overflow-x-auto sm:mx-4 flex-nowrap flex-row py-4 sm:flex-row sm:gap-4 sm:flex-wrap sm:block sm:columns-2">
+    <div className="flex overflow-x-auto sm:mx-4 flex-nowrap flex-row py-4 pr-4 sm:flex-row sm:gap-4 sm:flex-wrap sm:block sm:columns-2">
       <ul className="flex flex-row flex-shrink-0 sm:block sm:flex-shrink">
         {albumItems}
       </ul>
