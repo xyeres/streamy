ff38ae0db8f7e4cab91811e9150abd6574dcc322
Michael Carr <xyeres@me.com>
2022-03-02 10:11:20 -0800

    add og:width/height

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,8 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://streamy-nu.vercel.app" />
         <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/streamy-dev-51f11.appspot.com/o/static%2Fimages%2Fog%2FStreamyOGImage-1%400.75x.jpg?alt=media&token=dc57d86e-0b2c-4a7f-8913-d2ea3dd9a7fb" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="400" />
         <meta property="og:image:alt" content="Music streaming app screenshots" />
     </Head>
       <LayoutWrapper>
