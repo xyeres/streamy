e1987bc82f683c437a32c163849eca432f1cef03
Michael Carr <xyeres@me.com>
2022-03-01 20:22:51 -0800

    Add aspect-ratio to icon

--- components/Layout/AlbumCoverPlaceholder.js
+++ components/Layout/AlbumCoverPlaceholder.js
@@ -3,7 +3,7 @@ export default function AlbumCoverPlaceholder() {
   return (
     <div className="border-gray-300 grid place-items-center aspect-square bg-gray-200 border rounded-xl w-32 h-32">
       <span className="sr-only">Album placeholder</span>
-      <MdAlbum size="5em" />
+      <MdAlbum className="aspect-square" size="5em" />
     </div>
   )
 }
