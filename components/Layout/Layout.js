6e0c4f8d81ce8791639edc20c8a2504f57abe9c4
Michael Carr <xyeres@me.com>
2022-03-03 08:44:19 -0800

    Remove sm screen style

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,7 +3,7 @@ import GoBack from "./GoBack";
 export default function Layout({ children }) {
 
   return (
-    <main className={`w-full h-full flex flex-col sm:flex-row gap-2 sm:gap-7`}>
+    <main className={`w-full h-full flex flex-col gap-2 sm:gap-7`}>
       <>
         <GoBack />
         {children}
