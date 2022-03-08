27afe8c3cba33f8c7636c5c5fae66177cf580f1d
Michael Carr <xyeres@me.com>
2022-03-08 10:51:27 -0800

    Add pill to card instead of icon

--- components/Home/FeaturedCard.js
+++ components/Home/FeaturedCard.js
@@ -5,6 +5,7 @@ import { useFeatured } from '../CoverGrid/useAlbums';
 import { MdFeedback } from 'react-icons/md'
 import ErrMsgHome from '../Layout/ErrMsgHome'
 import LoadingFeaturedItem from '../Layout/LoadingFeaturedItem'
+import FeaturedPill from './FeaturedPill';
 
 export default function FeaturedCard({ item, featuredColl }) {
   const { data, isError, isLoading } = useFeatured(featuredColl)
@@ -17,12 +18,12 @@ export default function FeaturedCard({ item, featuredColl }) {
   return (
     <Link href={`album/${feature.id}`}>
       <a>
-        <div className="relative flex cursor-pointer justify-start group hover:bg-opacity-[65%] items-start m-4 sm:mx-4 bg-black bg-opacity-10 p-4 rounded-lg">
+        <div className="relative flex cursor-pointer justify-start group hover:bg-blue-500 items-start m-4 sm:mx-4 bg-black bg-opacity-5 p-4 rounded-lg">
           <Image src={feature.coverUrl} width={180} height={180} alt={feature.title} className="rounded-xl max-w-[96px] sm:max-w-[210px] aspect-square" />
           <div className="ml-4 mt-1 group-hover:text-white">
             <div className="flex flex-row items-start">
               <h3 className="font-bold">{feature.title}</h3>
-              <MdFiberNew size="1.35em" className="group-hover:text-white inline mt-1 ml-1" />
+              <div className="absolute right-2 -top-3"><FeaturedPill message="NEW" /></div>
             </div>
             <p className="text-sm mt-2">{feature.promo}</p>
           </div>
