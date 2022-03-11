b91cdf6f13d60784ffb479504cbf3f08f74b6bea
Michael Carr <xyeres@me.com>
2022-03-11 14:13:12 -0800

    Add large screen banner notice

--- components/Layout/LayoutWrapper.js
+++ components/Layout/LayoutWrapper.js
@@ -3,11 +3,10 @@ import MenuNav from "./MenuNav";
 
 export default function LayoutWrapper({ children, home, search, library, bareBones }) {
   return (
-    <div className={`
-      w-full
-      min-h-screen
-    `}>
-
+    <div className={`w-full min-h-screen`}>
+      <div className="hidden md:block p-4 bg-purple-400 text-white font-semibold text-center">
+        <p>App is not yet optimized for large screens, visit on your phone for best results!</p>
+      </div>
       {children}
 
       <Player />
