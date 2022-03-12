871c100480c5c580bd50c7279a2ac4f92e8f1f55
Michael Carr <xyeres@me.com>
2022-03-11 16:12:09 -0800

    Fix linting issues

--- components/Library/Feedback.js
+++ components/Library/Feedback.js
@@ -27,7 +27,7 @@ export default function Feedback({ isOpen, setIsOpen }) {
     if (feedback === "open") {
       setIsOpen(true)
     }
-  }, [feedback])
+  }, [feedback, setIsOpen])
 
 
   const handleHappy = (e) => {
@@ -155,77 +155,77 @@ export default function Feedback({ isOpen, setIsOpen }) {
           <div className="mt-10 transition-all bg-white text-whtie rounded-lg p-4">
             <details>
               <summary className="font-semibold text-lg">Feature Road Map</summary>
-              <div class="pt-6">
-                <div class="flex">
-                  <div class="flex flex-col items-center mr-4">
+              <div className="pt-6">
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
                     <div>
-                      <div class="flex items-center justify-center w-10 h-10 border rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
                           stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
                         </svg>
                       </div>
                     </div>
-                    <div class="w-px h-full bg-gray-300"></div>
+                    <div className="w-px h-full bg-gray-300"></div>
                   </div>
-                  <div class="pb-8">
-                    <p class="mb-2 text-xl font-bold text-gray-600">Step 1</p>
-                    <p class="text-gray-700">
+                  <div className="pb-8">
+                    <p className="mb-2 text-xl font-bold text-gray-600">Step 1</p>
+                    <p className="text-gray-700">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. veniam libero facilis minus reprehenderit.
                     </p>
                   </div>
                 </div>
-                <div class="flex">
-                  <div class="flex flex-col items-center mr-4">
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
                     <div>
-                      <div class="flex items-center justify-center w-10 h-10 border rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
                           stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
                         </svg>
                       </div>
                     </div>
-                    <div class="w-px h-full bg-gray-300"></div>
+                    <div className="w-px h-full bg-gray-300"></div>
                   </div>
-                  <div class="pb-8">
-                    <p class="mb-2 text-xl font-bold text-gray-600">Step 2</p>
-                    <p class="text-gray-700">
+                  <div className="pb-8">
+                    <p className="mb-2 text-xl font-bold text-gray-600">Step 2</p>
+                    <p className="text-gray-700">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. veniam libero facilis minus reprehenderit.
                     </p>
                   </div>
                 </div>
-                <div class="flex">
-                  <div class="flex flex-col items-center mr-4">
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
                     <div>
-                      <div class="flex items-center justify-center w-10 h-10 border rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
                           stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
                         </svg>
                       </div>
                     </div>
-                    <div class="w-px h-full bg-gray-300"></div>
+                    <div className="w-px h-full bg-gray-300"></div>
                   </div>
-                  <div class="pb-8 ">
-                    <p class="mb-2 text-xl font-bold text-gray-600">Step 3</p>
-                    <p class="text-gray-700">
+                  <div className="pb-8 ">
+                    <p className="mb-2 text-xl font-bold text-gray-600">Step 3</p>
+                    <p className="text-gray-700">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. veniam libero facilis minus reprehenderit.
                     </p>
                   </div>
                 </div>
-                <div class="flex">
-                  <div class="flex flex-col items-center mr-4">
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
                     <div>
-                      <div class="flex items-center justify-center w-10 h-10 border rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
                           stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
                         </svg>
                       </div>
                     </div>
                   </div>
-                  <div class="pt-1">
-                    <p class="mb-2 text-lg font-bold text-gray-600">Done</p>
+                  <div className="pt-1">
+                    <p className="mb-2 text-lg font-bold text-gray-600">Done</p>
                   </div>
                 </div>
               </div>
