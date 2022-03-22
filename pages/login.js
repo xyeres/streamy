b42abdec7026cad170bc0615d1e1d8f0b6525db3
Michael Carr <xyeres@me.com>
2022-03-22 09:54:16 -0700

    Using Firebase SDK

--- pages/login.js
+++ pages/login.js
@@ -30,30 +30,35 @@ export default function Login() {
 function SignInButtonGoogle() {
   const dispatch = useDispatch()
   const router = useRouter()
+
   const signInWithGoogle = async () => {
     signInWithPopup(auth, googleAuthProvider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
+
         // The signed-in user info.
         const user = result.user;
-
         setUserDoc(user)
         dispatch(loginUser(user))
         router.push('/profile')
+
         console.log(user)
-        // ...
+        
       }).catch((error) => {
         console.log(error)
+
         // Handle Errors here.
         const errorCode = error.code;
         const errorMessage = error.message;
+
         // The email of the user's account used.
         const email = error.email;
+
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
+        
       });
   }
 
