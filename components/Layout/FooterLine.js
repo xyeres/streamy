70c784673a5843af10b5cc1015edf7d8ca2c10cf
Michael Carr <xyeres@me.com>
2022-03-23 12:16:42 -0700

    Sweeten login screen

--- components/Layout/FooterLine.js
+++ components/Layout/FooterLine.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function FooterLine() {
   return (
-    <p className="text-xs mt-10 mx-20 text-center">
+    <p className="text-xs mx-10 text-center">
       By signing in you agree to our <Link href="/terms"><a className="link">terms of service</a></Link> and <Link href="/privacy"><a className="link">privacy policy</a></Link>.
     </p>
   )
--- pages/login.js
+++ pages/login.js
@@ -1,59 +1,56 @@
-import { getRedirectResult, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
-import { useSelector } from 'react-redux'
-import { selectUser } from '../features/user/userSlice'
-import { auth, googleAuthProvider } from '../src/firebase'
-import { Layout } from '../components/Layout'
-import setUserDoc from '../features/user/setUserDoc';
+import { getRedirectResult, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import { useRouter } from 'next/router';
-import FooterLine from '../components/Layout/FooterLine'
-import { useState } from 'react'
+import { useState } from 'react';
+import { FaGoogle } from 'react-icons/fa';
+import { GiSaloonDoors } from 'react-icons/gi';
+import { Layout } from '../components/Layout';
+import FooterLine from '../components/Layout/FooterLine';
+import setUserDoc from '../features/user/setUserDoc';
+import { auth, googleAuthProvider } from '../src/firebase';
 
 export default function Login() {
-  const [clicked, setclicked] = useState('not clicked')
   const [error, setError] = useState(null)
-  const user = useSelector(selectUser)
   const router = useRouter()
 
   const signInWithGoogle = async () => {
-    setclicked('yes clicked')
     try {
       await signInWithRedirect(auth, googleAuthProvider)
-    } catch(err) {
-      console.log(err)
-      setError(err)
+    } catch (err) {
+      setError(err.message)
     }
   }
-  
+
   getRedirectResult(auth)
     .then((result) => {
       if (result) {
         setUserDoc(result.user)
         router.push('/profile')
       }
-    }).catch((error) => {
-      console.error('Error using redirect result: ', error)
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
+    }).catch((err) => {
+      console.error('Error using redirect result:', err)
+      setError(err.message)
     });
 
   //   <Image alt='Sign in with Google' height={24} width={24} src={'/images/google.png'} />
   return (
     <Layout>
-      <div className="m-4 mt-16 flex flex-col items-center">
-        <h1 className="text-lg font-bold mb-10">Log in or create an account</h1>
-        <button
-          className="w-72 h-10 p-2 rounded-lg border border-gray-500 flex flex-row justify-center items-center font-semibold pl-2 text-sm hover:bg-purple-500 hover:text-white"
-          onClick={signInWithGoogle}>
-          Continue with Google
-        </button>
-        {clicked}
-        {error}
-        <FooterLine />
+      <div className="mt-10 flex flex-col items-center w-full">
+        <div className="max-w-lg p-4 flex flex-col items-center w-full mb-24 gap-6">
+          <GiSaloonDoors className="text-purple-500" size="8em" />
+          <h1 className="text-lg text-gray-700 font-bold">Log in or create an account</h1>
+          {
+            error && <div className="m-4 w-full p-2 py-4 rounded-sm border-red-400 border-dashed border ">
+              {error}
+            </div>
+          }
+          <button
+            className="w-full h-10 p-2 rounded-lg border hover:border-0 border-gray-500 flex flex-row justify-center items-center font-semibold text-sm hover:bg-purple-600 hover:text-white"
+            onClick={signInWithGoogle}>
+            <FaGoogle size="1.4em" />
+            <span className='pl-2'>Continue with Google</span>
+          </button>
+          <FooterLine />
+        </div>
       </div>
     </Layout>
   )
