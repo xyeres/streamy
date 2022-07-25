e08bdfac66179704de89ebdf346209f025842c2e
Michael Carr <xyeres@me.com>
2022-07-25 13:56:01 -0700

    Statically generate front page Fixes #35

--- components/CoverGrid/CoverGrid.js
+++ components/CoverGrid/CoverGrid.js
@@ -1,27 +1,14 @@
 import CoverGridItem from "./CoverGridItem";
-import useCollection, { useAlbums } from "./useAlbums";
-import ErrMsgHome from "../Layout/ErrMsgHome";
-import LoadingGrid from "../Layout/LoadingGrid";
 
-function CoverGrid({ coll, path, order, limit, swrkey, keywords }) {
-
-  let useDataFetcher;
-  if (path === '/album') useDataFetcher = () => useAlbums(keywords, order, limit, swrkey)
-  if (path === '/playlist') useDataFetcher = () => useCollection(coll, order)
-
-  const { data, isLoading, isError } = useDataFetcher()
-
-  if (isError) return <ErrMsgHome message={isError.message} />
-  if (isLoading) return <LoadingGrid />
-
-  const albumItems = data.map((item, index) => {
+function CoverGrid({ items, path }) {
+  const listItems = items.map((item, index) => {
     return <CoverGridItem key={index} path={path} item={item} />
   })
 
   return (
     <div className="flex overflow-x-auto flex-nowrap flex-row sm:flex-col py-4 pr-4 lg:overflow-x-visible">
       <ul className="flex flex-row  gap-4 first:ml-4 flex-shrink-0 lg:flex-wrap">
-        {albumItems}
+        {listItems}
       </ul>
     </div>
   );
