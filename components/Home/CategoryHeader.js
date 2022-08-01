22b571571225fefb538e69ec1d3606b76b3b5874
Michael Carr <xyeres@me.com>
2022-08-01 09:57:56 -0700

    Adjust padding

--- components/Home/CategoryHeader.js
+++ components/Home/CategoryHeader.js
@@ -1,5 +1,5 @@
 export default function CategoryHeader({ title }) {
   return (
-    <h2 className="text-slate-600 font-bold mt-5 mx-4 lg:mx-8 text-lg lg:text-xl lg:bottom-line">{title}</h2>
+    <h2 className="text-slate-600 font-bold mt-5 pt-3 mx-4 border-solid border-t border-t-zinc-100 text-lg lg:text-xl lg:bottom-line">{title}</h2>
   );
 }
