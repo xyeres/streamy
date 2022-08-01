30d778554566e7fac564974fef13f1af59c78ff3
Michael Carr <xyeres@me.com>
2022-08-01 09:34:15 -0700

    Remove nested tags

--- components/Home/CategoryHeader.js
+++ components/Home/CategoryHeader.js
@@ -1,7 +1,5 @@
-export default function CategoryHeader({title}) {
+export default function CategoryHeader({ title }) {
   return (
-    <div className="relative">
-      <h2 className="text-slate-600 font-bold mt-5 mx-4 text-lg lg:text-xl lg:bottom-line">{title}</h2>
-    </div>
+    <h2 className="text-slate-600 font-bold mt-5 mx-4 lg:mx-8 text-lg lg:text-xl lg:bottom-line">{title}</h2>
   );
 }
